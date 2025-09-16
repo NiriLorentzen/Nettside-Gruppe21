9552fdf62e96bb40405bb53da234dbf4ab4d0321
Eivind Hovden Breivik <eivindhb@uia.no>
2025-09-16 13:07:11 +0200

    Changed member class

--- main.js
+++ main.js
@@ -23,7 +23,9 @@ const members = [
   {
     name: "Niri August Lorentzen",
     role: "Prosjektleder",
-    description: `Jeg kommer fra Lillestrøm og er 21 år gammel. Jeg er lederen for gruppen, og har stor interesse for backend og frontend utvikling.
+    age: `21`,
+    origin: `Lillestrøm`,
+    description: `Jeg er lederen for gruppen, og har stor interesse for backend og frontend utvikling.
     <br> Jeg har alltid hatt stor interesse for data og IT, og gjennom studiet har jeg fått erfaring og kunnskap innen organisering av prosjektarbeid, frontend utvikling og backend utvikling.
     <br> Her er det backend og databasearbeid jeg har likt mest og fått best resultater ifra, med bruk av teknologier som PostgreSQL, Docker og Entity Framework.
     <br> Jeg har også mye interesse for frontend og har også drevet en del med Figma for designutvikling under prosjektarbeid. 
@@ -38,7 +40,9 @@ const members = [
   {
     name: "Eivind Hovden Breivik",
     role: "Backend utvikler",
-    description: `Jeg er 25 år gammel og kommer fra Kongsberg, med stor interesse i både maskin og programvare. 
+    age: `25`,
+    origin: `Kongsberg`,
+    description: `Jeg har stor interesse i både maskin og programvare. 
     <br> Gjennom IT-studiet har jeg tilegnet meg erfaring i frontend- og backend-utvikling, både teoretisk og praktisk. 
     <br> Evnene ble utviklet med en rekke prosjekter med bruk av teknologier som: Java, C#, Python, PostgreSQL og nå også PHP.
     <br> Å gjøre disse prosjektene har gitt meg en spesiell interesse for optimalisering av kode og programmer. 
@@ -52,7 +56,9 @@ const members = [
   {
     name: "William Jondahl",
     role: "Backend utvikler",
-    description: `Jeg er en 21 år gammel student fra Drammen, og er interessert i cybersikkerhet, AI og utvikling. Jeg har praktisk erfaring med C#, Java, PostgreSQL med fokus på webutvikling. 
+    age: `21`,
+    origin: `Drammen`,
+    description: `Jeg er interessert i cybersikkerhet, AI og utvikling. Jeg har praktisk erfaring med C#, Java, PostgreSQL med fokus på webutvikling. 
     <br> Gjennom studiene har jeg tilegnet team-arbeidsferdigheter som smidig utvikling med Scrum og versjonskontroll med Git.
     <br> Jeg har blitt særlig interessert i cybersikkerhet og vil gjerne utvikle ferdighetene mine innen dette feltet.`,
     picture: "images/williamj.jpg",
@@ -63,7 +69,9 @@ const members = [
   {
     name: "Carl Andreas Arnegard",
     role: "Frontend utvikler",
-    description: `Hei, jeg er 21 år gammel og kommer fra Drammen. Jeg har lenge vært interessert i teknologi og programmering, 
+    age: `21`,
+    origin: `Drammen`,
+    description: `Jeg har lenge vært interessert i teknologi og programmering, 
     <br>og har erfaring med flere programmeringsspråk som Python, Java, C# og jeg har også jobbet med webutvikling ved bruk av HTML, CSS og JavaScript.
     <br>Jeg studerer nå IT og Informasjonssystemer ved Universitetet i Agder, hvor jeg har fått muligheten til å jobbe med ulike prosjekter som har styrket mine ferdigheter innen både frontend- og backend-utvikling.`,
     picture: "images/CarlBilde.JPG",
@@ -74,7 +82,9 @@ const members = [
   {
     name: "Nicolay Lehne-Torp",
     role: "Fullstack utvikler",
-    description: `Jeg er 21 år gammel og kommer fra Drammen. Jeg har funnet en stor interesse for å skape nettsider og applikasjoner gjennom ulike prosjekter i studiene.
+    age: `21`,
+    origin: `Drammen`,
+    description: `Jeg har funnet en stor interesse for å skape nettsider og applikasjoner gjennom ulike prosjekter i studiene.
     <br>Jeg har tidligere erfaring med HTML, CSS, Java, JavaScript, Python, C#, PostgreSQL og nå også PHP. Jeg er en person som liker å jobbe i team og har gode samarbeidsevner.`,
     picture: "images/NicolayBilde.JPG",
     linkedin: "https://www.linkedin.com/in/nicolay-lehne-torp-14b62a2ba/",
@@ -94,7 +104,9 @@ function renderMembers() {
       <img class="Personbilde" src="${m.picture}" alt="Bilde av ${m.name}">
       <text class="medlem_instans_tekst_boks">
         <div>
-          <p><strong>${m.name}</strong> – <em>${m.role}</em></p>
+          <p><strong>${m.name}</strong></p>
+          <p><em>${m.role}</em></p>
+          <p> ${m.age} år - ${m.origin}</p>         
           ${m.description}
         </div>
       </text>
